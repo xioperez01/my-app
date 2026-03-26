43fb14897bdc8efb9e587c7101b9e4578725a2a6
xioperez01 <angie.xioperez@gmail.com>
2026-03-25 21:49:31 -0500

    Add pagination to AdvisorsTable component for improved data navigation

--- src/components/advisors/AdvisorsTable.tsx
+++ src/components/advisors/AdvisorsTable.tsx
@@ -1,6 +1,8 @@
 "use client";
+import { usePagination } from "@/hooks/usePagination";
 import { useSort } from "@/hooks/useSort";
 import { Advisor } from "@/types/advisor";
+import Paginator from "@/components/Paginator";
 
 function AdvisorsTable({ advisors }: { advisors: Advisor[] }) {
   const { sortedData, handleSort, sortKey, sortOrder } = useSort(
@@ -8,27 +10,40 @@ function AdvisorsTable({ advisors }: { advisors: Advisor[] }) {
     "name",
   );
 
+  const { paginatedData, currentPage, next, prev, hasNext, hasPrev, label } =
+    usePagination<Advisor>(sortedData);
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th onClick={() => handleSort("name")}>
-            Name {sortKey === "name" ? sortOrder : ""}
-          </th>
-          <th onClick={() => handleSort("income")}>
-            Income {sortKey === "income" ? sortOrder : ""}
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {sortedData?.map((advisor) => (
-          <tr key={advisor?.id}>
-            <td>{advisor?.name}</td>
-            <td>${advisor?.income}</td>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th onClick={() => handleSort("name")}>
+              Name {sortKey === "name" ? sortOrder : ""}
+            </th>
+            <th onClick={() => handleSort("income")}>
+              Income {sortKey === "income" ? sortOrder : ""}
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {paginatedData?.map((advisor) => (
+            <tr key={advisor?.id}>
+              <td>{advisor?.name}</td>
+              <td>${advisor?.income}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Paginator
+        currentPage={currentPage}
+        next={next}
+        prev={prev}
+        hasNext={hasNext}
+        hasPrev={hasPrev}
+        label={label}
+      />
+    </div>
   );
 }
 
