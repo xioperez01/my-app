6433b4b3964410dd1d2977b9e3303f19ddb2ee2e
xioperez01 <angie.xioperez@gmail.com>
2026-03-26 12:25:14 -0500

    Add layout styles and structure for HomeLayout component

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,11 +1,15 @@
+import Navbar from "@/components/Navbar/Navbar";
+import styles from "./layout.module.css";
+
 export default function HomeLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <main className="flex items-center justify-center min-h-screen">
-      {children}
+    <main className={styles.root}>
+      <Navbar />
+      <div className={styles.container}>{children}</div>
     </main>
   );
 }
