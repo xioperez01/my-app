5caa62d2f6e5a4c8a1c056445c615316bd334f6d
xioperez01 <angie.xioperez@gmail.com>
2026-03-26 17:37:23 -0500

    Add AdvisorPage component with layout, actions, and data display

--- src/app/(dashboard)/advisors/[id]/page.tsx
+++ src/app/(dashboard)/advisors/[id]/page.tsx
@@ -1,5 +1,20 @@
 import { getAdvisorById } from "@/lib/advisor";
 import { notFound } from "next/navigation";
+import styles from "./page.module.css";
+import Image from "next/image";
+import { Button } from "@/components/ui/Button/Button";
+import LocationIcon from "@/components/icons/LocationIcon";
+import BagIcon from "@/components/icons/BagIcon";
+import { Advisor } from "@/types/advisor";
+
+const dataToDisplay: { key: keyof Advisor; label: string }[] = [
+  { key: "idNumber", label: "ID Number" },
+  { key: "email", label: "Email" },
+  { key: "income", label: "Income" },
+  { key: "education", label: "Education" },
+  { key: "title", label: "Title" },
+  { key: "experienceYears", label: "Years of Experience" },
+];
 
 export default async function AdvisorPage({
   params,
@@ -13,5 +28,43 @@ export default async function AdvisorPage({
     notFound();
   }
 
-  return <div>Advisor: {advisor.name}</div>;
+  return (
+    <div className={styles.root}>
+      <div className={styles.topContainer}>
+        <Image
+          src={advisor.avatar}
+          alt={advisor.name}
+          width={112}
+          height={112}
+          className={styles.avatar}
+        />
+        <div className={styles.actionsContainer}>
+          <Button colorScheme="danger">Delete</Button>
+          <Button variant="outline" colorScheme="primary">
+            Edit Advisor
+          </Button>
+        </div>
+      </div>
+      <div className={styles.primaryInfoContainer}>
+        <h1>{advisor.name}</h1>
+        <div className={styles.rowContainer}>
+          <LocationIcon />
+          <span>{advisor.address}</span>
+        </div>
+        <div className={styles.rowContainer}>
+          <BagIcon />
+          <span>Zoe Advisors</span>
+        </div>
+      </div>
+      <hr />
+      <div className={styles.dataContainer}>
+        {dataToDisplay.map((item) => (
+          <div key={item.key}>
+            <span>{item.label}: </span>
+            <span>{advisor[item.key] || "No data"}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
