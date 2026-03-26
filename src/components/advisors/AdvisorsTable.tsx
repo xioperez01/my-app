fcb1a0a73cdaf7813dae2b00020b36aaee4ae599
xioperez01 <angie.xioperez@gmail.com>
2026-03-25 22:16:12 -0500

    Refactor AdvisorsTable to  enhance row click functionality for navigating to advisor details

--- src/components/advisors/AdvisorsTable.tsx
+++ src/components/advisors/AdvisorsTable.tsx
@@ -2,21 +2,27 @@
 import { Advisor } from "@/types/advisor";
 import Paginator from "@/components/Paginator";
 import { useTableState } from "@/hooks/useTable";
+import { useRouter } from "next/navigation";
 
 function AdvisorsTable({ advisors }: { advisors: Advisor[] }) {
-const {
-  data,
-  sortKey,
-  sortOrder,
-  handleSort,
-  currentPage,
-  next,
-  prev,
-  hasNext,
-  hasPrev,
-  label,
-} = useTableState(advisors, "name");
+  const router = useRouter();
 
+  const {
+    data,
+    sortKey,
+    sortOrder,
+    handleSort,
+    currentPage,
+    next,
+    prev,
+    hasNext,
+    hasPrev,
+    label,
+  } = useTableState(advisors, "name");
+
+  const handleGoToDetail = (id: string) => {
+    router.push(`/advisors/${id}`);
+  };
   return (
     <div>
       <table>
@@ -32,7 +38,7 @@ const {
         </thead>
         <tbody>
           {data?.map((advisor) => (
-            <tr key={advisor?.id}>
+            <tr key={advisor?.id} onClick={() => handleGoToDetail(advisor?.id)}>
               <td>{advisor?.name}</td>
               <td>${advisor?.income}</td>
             </tr>
