4a70bc227cbd6d1d71d2708797e3426d895c09bf
xioperez01 <angie.xioperez@gmail.com>
2026-03-26 16:46:16 -0500

    Add LeftArrowIcon, RightArrowIcon, and TableSortIcon components with styles

---
+++ src/components/icons/RightArrowIcon.tsx
@@ -0,0 +1,30 @@
+function RightArrowIcon() {
+  return (
+    <svg
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_1278_193)">
+        <g clip-path="url(#clip1_1278_193)">
+          <path
+            d="M3.66126 13.8685C3.55383 13.7704 3.50014 13.6361 3.50014 13.5014C3.50014 13.3798 3.54408 13.2582 3.63295 13.162L9.29221 7.00275L3.63283 0.839449C3.44533 0.636328 3.45705 0.31946 3.66115 0.132902C3.86427 -0.0545941 4.17957 -0.0419069 4.36707 0.160732L10.3669 6.66059C10.5447 6.85296 10.5447 7.14683 10.3669 7.33933L4.36707 13.8392C4.17875 14.0404 3.86438 14.056 3.66126 13.8685Z"
+            fill="#8B8B8B"
+          />
+        </g>
+      </g>
+      <defs>
+        <clipPath id="clip0_1278_193">
+          <rect width="14" height="14" fill="white" />
+        </clipPath>
+        <clipPath id="clip1_1278_193">
+          <rect width="14" height="14" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export default RightArrowIcon;
